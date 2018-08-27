6effd32760533a8b60b0516b91de9eb73cbf2d4a
yura <funny.falcon@gmail.com>
2018-08-27 20:19:53 +0300

    Option for "migrate from slave" behavior.
    Previously (and by default after commit) when master loose its last slot
    (due to migration, for example), its slaves will migrate to new last slot
    holder.
    It causes timeouts in production, because new master will have to send
    whole its dump to those slaves.
    Also it leads to cluster topology mutation, that is inconvenient if
    topology is managed by external tools.
    
    This commit adds 'cluster-migrate-from-empty' configuration option that is
    enabled by default to preserve existed behavior. When disabled, slave will
    not migrate when master became empty.
    
    fixes #4896

--- src/cluster.c
+++ src/cluster.c
@@ -1596,7 +1596,7 @@ void clusterUpdateSlotsConfigWith(clusterNode *sender, uint64_t senderConfigEpoc
      *    master.
      * 2) We are a slave and our master is left without slots. We need
      *    to replicate to the new slots owner. */
-    if (newmaster && curmaster->numslots == 0) {
+    if (server.cluster_migrate_from_empty && newmaster && curmaster->numslots == 0) {
         serverLog(LL_WARNING,
             "Configuration change detected. Reconfiguring myself "
             "as a replica of %.40s", sender->name);
--- src/cluster.h
+++ src/cluster.h
@@ -22,6 +22,7 @@
 #define CLUSTER_FAIL_UNDO_TIME_ADD 10 /* Some additional time. */
 #define CLUSTER_FAILOVER_DELAY 5 /* Seconds */
 #define CLUSTER_DEFAULT_MIGRATION_BARRIER 1
+#define CLUSTER_DEFAULT_MIGRATE_FROM_EMPTY 1 /* Migrate slave when master becomes empty by default. */
 #define CLUSTER_MF_TIMEOUT 5000 /* Milliseconds to do a manual failover. */
 #define CLUSTER_MF_PAUSE_MULT 2 /* Master pause manual failover mult. */
 #define CLUSTER_SLAVE_MIGRATION_DELAY 5000 /* Delay for slave migration. */
